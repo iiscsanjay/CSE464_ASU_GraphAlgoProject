f3062de57eafd733442ec66a2bd3bfb3211e1b07
sanjayk <iet.sanjay@gmail.com>
2025-05-02 14:33:47 +0530

    Feature 1 : updated for Prting Nodes, Edges and Direction

--- src/main/java/MyGraphApp.java
+++ src/main/java/MyGraphApp.java
@@ -36,8 +36,22 @@ public class MyGraphApp {
         });
         dotImporter.importGraph(directedGraph, new StringReader(contentBuilder.toString()));
 
-        //Print the parse grpah
-        System.out.println(toString());
+        // Printing the Number of Nodes
+        System.out.println("Number of Nodes: " + directedGraph.vertexSet().size());
+        for(String v : directedGraph.vertexSet() ) {
+            System.out.println("\tLabel of Node : " + v);
+        }
+        // Printing the Number of Edges 
+        System.out.println("Number of Edges: " + directedGraph.edgeSet().size());
+        //for(String v : directedGraph.vertexSet() ) {
+        //    System.out.println("\tNodes: Label of Node : " + v);
+        //}
+        for(DefaultEdge e : directedGraph.edgeSet() ) {
+            String source = directedGraph.getEdgeSource(e);
+            String target = directedGraph.getEdgeTarget(e);
+            System.out.println("\tEdge : " + source + " -> " + target);
+        }
+        
     }
 
     public String toString() {
@@ -55,8 +69,5 @@ public class MyGraphApp {
             exporter.exportGraph(directedGraph, new FileWriter(filepath));
         }catch (IOException e){
         }
-        //Writer writer = new StringWriter();
-        //exporter.exportGraph(directedGraph, writer);
-        //System.out.println(writer.toString());
     }
 }
